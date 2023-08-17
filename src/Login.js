cc6491a76a51d88adc0cead3d141a92312e2c5f9
Akshay Gulhane <argulhane10246@gmail.com>
2023-08-17 22:52:21 +0530

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+import add from "../img/add.png"
+function Login() {
+  return (
+    <>
+
+    <div className="formContainer">
+        <div className="formWrapper">
+        <span className="logo"> Free Chat</span>
+        <span className="title">Login</span>
+       
+            <form>
+                
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>
+                <button>Sign In</button>
+            </form>
+            <p> You Dont have an account ? Sign up</p>
+
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default Login;
