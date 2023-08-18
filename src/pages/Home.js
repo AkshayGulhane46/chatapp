d266a4c09ae37911dee40c32bbdeccb0ba8d4ec3
Akshay Gulhane <argulhane10246@gmail.com>
2023-08-18 23:21:24 +0530

    Commit180823

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './Login';
+import Register from './Register';
 import "./style.scss"
 
 function App() {
