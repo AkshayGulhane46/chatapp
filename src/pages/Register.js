50a9b21409a27f882ea803901bd8b6cc3b44b0bf
Akshay Gulhane <argulhane10246@gmail.com>
2023-08-25 22:55:23 +0530

    Commit2508231

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,54 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import add from "../img/add.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+import {auth , storage } from "../firebase"
+
 function Register() {
+  const [err,setErr] = useState(false);
+    const handleSubmit = async (e) =>{ 
+    console.log("under handleSubmit");
+    e.preventDefault();
+    console.log(e.target[0].value);
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0]; // to get the profile pic
+
+
+try{
+  const res = await createUserWithEmailAndPassword(auth, email, password)
+  
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+uploadTask.on(
+
+  (error) => {
+    // Handle unsuccessful uploads
+    setErr(true);
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      await updateProfile(res.user);
+      console.log('File available at', downloadURL,{
+        displayName, 
+        photoURL : downloadURL 
+      });
+    });
+  }
+);
+
+
+}catch(err){
+  setErr(true);
+}
+  
+  }
   return (
     <>
 
@@ -10,7 +57,7 @@ function Register() {
         <span className="logo"> Free Chat</span>
         <span className="title">Register</span>
        
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="display name"/>
                 
                 <input type="email" placeholder="email"/>
@@ -22,7 +69,7 @@ function Register() {
                 <img src={add}/>
                 <span> Add your Image </span> </label>
                 <button>Sign Up</button>
-
+                {err && <span>Something went wrong </span>}
 
             </form>
             <p> Do you have an account ? login</p>
